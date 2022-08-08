e79eebd077f5bdfc6dd037f40c32b5e6b4660543
Anshi Mathur <92960387+anshimathur0325@users.noreply.github.com>
2022-08-08 15:26:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def predict():
     return render_template("index.html", prediction_text=str)
 
 if __name__ == "__main__":
-    flask_app.run(debug=True, host='0.0.0.0', port=80)
+    flask_app.run(debug=True)
