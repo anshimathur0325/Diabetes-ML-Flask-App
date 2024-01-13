b76d801cf37418c03380ca58672d9ee848b4c29e
anshimathur0325 <anshimathur0325@gmail.com>
2024-01-12 23:40:01 -0600

    try out different models

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score
 import pickle
 
@@ -17,7 +17,7 @@ X = df[features]
 y = df["Outcome"]
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25,stratify=df.Outcome, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, stratify=df.Outcome, random_state=0)
 
 # Feature scaling (not always necessary for Random Forest, but can be done for consistency)
 scaler = StandardScaler()
@@ -25,13 +25,13 @@ X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
 # Choose a classifier (Random Forest in this case)
-classifier = RandomForestClassifier(n_estimators=100, random_state=0)
+classifier = RandomForestClassifier(n_estimators=150, max_depth=20, min_samples_split=5, min_samples_leaf=2, random_state=0)
 
 # Train the classifier
-classifier.fit(X_train, y_train)
+classifier.fit(X_train_scaled, y_train)
 
 # Make predictions on the test set
-predictions = classifier.predict(X_test)
+predictions = classifier.predict(X_test_scaled)
 
 # Evaluate the accuracy of the model
 accuracy = accuracy_score(y_test, predictions)
